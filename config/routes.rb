28a0344d643d44fdd78518c6ad5dd44d319d2f9f
Naoto3615 <63333757+Naoto3615@users.noreply.github.com>
2020-09-08 10:07:43 +0900

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'todos', to: 'todos#index'
+  post 'todos', to: 'todos#create'
+  get 'todos/new', to: 'todos#new',as: 'new_todo'
+  get 'todos/:id/edit', to: 'todos#edit',as: 'edit_todo'
+  get 'todos/:id', to: 'todos#show',as: 'todo'
+  patch 'todos/:id', to: 'todos#update'
+  put 'todos/:id', to: 'todos#update'
+  delete 'todos/:id', to: 'todos#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
